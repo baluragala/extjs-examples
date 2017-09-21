71197d0c8d1a40c776e31d5d7793f68d87ad7c46
baluragala <baluragala@gmail.com>
2017-09-21 08:15:12 +0530

    Alchology app added

---
+++ Alcohology/app/view/main/Main.js
@@ -0,0 +1,32 @@
+// app/view/main/Main.js
+Ext.define('Alcohology.view.main.Main', {
+    extend: 'Ext.Panel',
+    xtype: 'app-main',
+
+    requires:[
+        'Alcohology.view.main.MainController',
+        'Alcohology.view.main.MainModel',
+    ],
+
+    controller: 'main',
+    viewModel: 'main',
+    layout: 'border',
+
+    header: { xtype: 'app-header' },
+    items: [
+        { xtype: 'categories', width: 200, region: 'west' },
+        { xtype: 'product-list', region: 'center' }
+    ],
+
+    initComponent: function() {
+        this.callParent(arguments);
+
+        this.add(Ext.create('Alcohology.view.cart.Cart', {
+            reference: 'cartWindow'
+        }));
+
+        this.add(Ext.create('Alcohology.view.account.Account', {
+            reference: 'accountWindow'
+        }));
+    }
+});
